5250bb4583965fb4cdcff8683af8c825be33c26a
AnonDax <anondax.dev@gmail.com>
2019-01-20 12:55:40 +0100

    feat : Download emojis and server's icon

--- src/file/GuildFile.py
+++ src/file/GuildFile.py
@@ -1,10 +1,31 @@
 import json
+import requests
+
+import os
 
 class GuildFile:
-	def __init__(self):
-		pass;
+    def __init__(self):
+    	pass;
 
 
-	def saveGuild(self, guildModel):
-		file = open("guilds/"+guildModel.id+"_"+guildModel.name+".json", "w");
-		file.write(json.dumps(guildModel.__dict__, indent=4));
+    def saveGuild(self, guildModel):
+        basePath = "guilds/"+guildModel.id+"_"+guildModel.name+"/";
+
+        try:
+            os.makedirs(basePath+"emojis");
+        except OSError as exc:
+            pass;
+
+        file = open(basePath+guildModel.name+".json", "w");
+        file.write(json.dumps(guildModel.__dict__, indent=4));
+
+        self.savePicture(guildModel.icon_url, basePath+guildModel.icon);
+
+        for emoji in guildModel.emojis:
+            self.savePicture(emoji["url"], basePath+"emojis/"+emoji["id"]);
+
+
+
+    def savePicture(self, url, path):
+        r = requests.get(url, allow_redirects=True);
+        open(path+"."+url[url.rfind(".")+1:], 'wb').write(r.content);
