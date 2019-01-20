e6bb56190f784c7891d6e0e9b60def1f17bfbe65
AnonDax <anondax.dev@gmail.com>
2019-01-20 22:05:45 +0100

    feat : Remove unused field in EmojiModel

--- src/model/EmojiModel.py
+++ src/model/EmojiModel.py
@@ -2,14 +2,10 @@ class EmojiModel:
     def __init__(self):
         self.id = None;
         self.name = None;
-        self.require_colons = None;
-        self.managed = None;
         self.url = None;
-    
+
 
     def fillFromEmoji(self, emoji):
         self.id = emoji.id;
         self.name = emoji.name;
-        self.require_colons = emoji.require_colons;
-        self.managed = emoji.managed;
         self.url = emoji.url;
